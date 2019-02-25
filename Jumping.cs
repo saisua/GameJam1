ba2c2bb847e6337bc158b2953974af2fdf8adedb
TheMuiaio <47815848+TheMuiaio@users.noreply.github.com>
2019-02-25 11:51:19 +0100

    Add files via upload

--- Jumping.cs
+++ Jumping.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour
+public class Jumping : MonoBehaviour
 {
+    public AudioClip jumpSound;
+
+
+    private float throwSpeed = 5000f;
+    private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            source.PlayOneShot(jumpSound, 1);
+        }
+
     }
 }
+
